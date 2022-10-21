7deb308f679136f21503eae8a7d89a333082c4b5
Guvanch99 <awediyewguwanc@gmail.com>
2022-10-21 14:48:39 +0300

    fix blog

--- src/features/blog/components/Blog.tsx
+++ src/features/blog/components/Blog.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components/macro'
 import Card from './Card'
-import Image from '../../../core/assets/incoterms.png'
+import Image from '../../../core/assets/incoterms.jpeg'
 
 const WrapperStyled = styled.div`
   width: 100%;
